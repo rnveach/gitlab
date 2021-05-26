8c81fd47db35ea588c3fd76cf6f99ea7dcc78482
Avielle Wolfe <awolfe@gitlab.com>
2021-05-26 20:58:56 +0200

    Fix audit service namespace
    The specs were passing, but my local development environment errored
    because it couldn't locate the class

--- ee/app/services/app_sec/dast/profiles/update_service.rb
+++ ee/app/services/app_sec/dast/profiles/update_service.rb
@@ -11,7 +11,7 @@ module AppSec
           return error('Profile parameter missing') unless dast_profile
 
           old_params = dast_profile.attributes.symbolize_keys
-          auditor = Audit::UpdateService.new(container: container, current_user: current_user, params: {
+          auditor = AppSec::Dast::Profiles::Audit::UpdateService.new(container: container, current_user: current_user, params: {
             dast_profile: dast_profile,
             new_params: dast_profile_params,
             old_params: old_params
